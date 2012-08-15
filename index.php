66d9cebaf41af7391a4d116a0099525a5f5462b6
Frank <cfwei@Frank-Ubuntu.(none)>
2012-08-16 02:37:06 +0800

    index login and register finish

--- index.php
+++ index.php
@@ -1,71 +1,11 @@
-<?php
-$content="";
-$ID=$_POST['ID'];
-$PASSWD=$_POST['PASSWD'];
 
-if($ID!=""&&$PASSWD!="")
-{	
-	require_once("session.php");
-	require_once("connect_mysql_class.php");
-	require_once("mysql_inc.php");	
-	$content="123";
-	$db=new DB();
-	$db->connect_db($_DB['host'], $_DB['username'], $_DB['password'], $_DB['dbname']);
-
-	$query="SELECT * FROM store_information where `UserID` =  '".$ID."' AND `UserPassword` ='".$PASSWD."'";
-	$db->query($query);
-
-	if($db->get_num_rows()!=1)
-	{	
-		$content="登入失敗";
-	}
-	else
-	{
-		$result=$db->fetch_array();
-		$se=new session();
-		$se->register_value("USERID",$_POST['ID']);
-		$se->register_value("SerialNumbers",$result['SerialNumbers']);
-		header("Location:manager.php");
-	}
-
-}
-
-
-
-?>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html" charset="UTF-8">
 <link rel=stylesheet type="text/css" href="style.css">
 <script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
-<script type="text/javascript">
-
-function gopage(choose)
-{	
-
-
-	if(choose==1)
-	{
-		document.getElementById('LoginBox').action="index.php";
-		document.LoginBox.submit();	
-	}
-	if(choose==2)
-	{	
-		$('#content').animate({
-				height:"60%"
-			      },1500);
-		$('#content').load('register.html');
-
-		//document.getElementById('LoginBox').action="register.html";
-		//document.LoginBox.submit();	
-	}
-	
-}
-
-
-</script>
-
-
+<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+<script type="text/javascript" src="function.js"></script>
 </head>
 <body>
 <div id="main">
@@ -79,7 +19,7 @@ function gopage(choose)
 
 	</div>
 	<script>
-		$('#content').load('login.html')
+		$('#content').load('login.php');
 	</script>
 	<div id="footer">
 		Copyright (c) 2012 Sitename.com. All rights reserved. Design by FCT.		
--- login.php
+++ login.php
@@ -1,31 +1,69 @@
-<?php 
-require_once("session.php");
-require_once("connect_mysql_class.php");
-require_once("mysql_inc.php");
-
-echo '<meta http-equiv="Content-Type" content="text/html" charset="UTF-8">';
-
-$db=new DB();
-$db->connect_db($_DB['host'], $_DB['username'], $_DB['password'], $_DB['dbname']);
-
-$query="SELECT * FROM store_information where `UserID` =  '".$_POST['ID']."' AND `UserPassword` ='".$_POST['passwd']."'";
-$db->query($query);
-
-if($db->get_num_rows()!=1)
+<?php
+$content="";
+$ID=$_POST['ID'];
+$PASSWD=$_POST['PASSWD'];
+if($ID!=""&&$PASSWD!="")
 {	
-		echo "登入失敗!";
-		header("Refresh: 1; url=\"index.html\"");
-		exit;
-}	
-else
-	echo "登入成功!";
+	require_once("session.php");
+	require_once("connect_mysql_class.php");
+	require_once("mysql_inc.php");	
+	$content="123";
+	$db=new DB();
+	$db->connect_db($_DB['host'], $_DB['username'], $_DB['password'], $_DB['dbname']);
 
-$result=$db->fetch_array();
+	$query="SELECT * FROM store_information where `UserID` =  '".$ID."' AND `UserPassword` ='".$PASSWD."'";
+	$db->query($query);
 
+	if($db->get_num_rows()!=1)
+	{	
+		$content="登入失敗";
+	}
+	else
+	{
+		$result=$db->fetch_array();
+		$se=new session();
+		$se->register_value("USERID",$_POST['ID']);
+		$se->register_value("SerialNumbers",$result['SerialNumbers']);
+		$content="登入成功";
+		echo '<script>
+			$("#content").animate({
+				height:"500px",
+				width:"1000px",
+				left:"3%",
+			      },800);
+		$("#content").load("register.html")</script>';	
+		exit;		
+		//header("Location:manager.php");
+	}
 
-$se=new session();
-$se->register_value("USERID",$_POST['ID']);
-$se->register_value("SerialNumbers",$result['SerialNumbers']);
-header("Location:manager.php");
+}
 
 ?>
+
+<div id="centerbox" class="ContentStyle">
+	<div id="hint">
+		<?php
+			echo $content;
+		?>
+	</div>
+	<div id=CenterContent">
+		<form id="LoginBox" method="post" action="">
+			<div>
+				<p>帳號:<input type="text" class="LoginTextArea" id="ID"></p>
+				<p>密碼:<input type="password" class="LoginTextArea" id="PASSWD"></p>
+			</div>
+		</form>
+		<button id="LoginButton" class="ButtonStyle" >登入</button>
+		<button id="RegisterButton" class="ButtonStyle" onclick="javascript:gopage(2)">註冊</button>	
+		<script>
+			$('#LoginButton').click(
+			function()
+			{	
+				var ID=$('#ID').val();
+				var PASSWD=$('#PASSWD').val();
+				$('#content').load('login.php',{"ID":ID,"PASSWD":PASSWD});
+			});
+		</script>
+	</div>
+</div>
+
