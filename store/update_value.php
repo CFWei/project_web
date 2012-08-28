d11f8148eb5fe3dff3c22731ee22317c725dc887
cfwei <s124210@gmail.com>
2012-08-29 02:49:52 +0800

    modify updateValue

--- store/update_value.php
+++ store/update_value.php
@@ -3,11 +3,11 @@
 require_once("../connect_mysql_class.php");
 require_once("../mysql_inc.php");
 
-//$SerialNumbers=$_POST['SerialNumbers'];
-//$ID=$_POST['ID'];
+$SerialNumbers=$_POST['SerialNumbers'];
+$ID=$_POST['ID'];
 
-$SerialNumbers="j5I5hh56nZVNqG3N4WcM";
-$ID="7PQSzAnpec";
+//$SerialNumbers="j5I5hh56nZVNqG3N4WcM";
+//$ID="7PQSzAnpec";
 
 $db=new DB();
 $db->connect_db($_DB['host'], $_DB['username'], $_DB['password'], $_DB['dbname']);
